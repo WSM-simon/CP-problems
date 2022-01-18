6c61df7e60d0b3a5658c4ff5a9a0e77d88f49d9b
WSM-simon <72824231+WSM-simon@users.noreply.github.com>
2022-01-18 19:57:54 -0400

    add Mouse Journey.cpp

--- CCC/CCC 12/CCC 12 S5 - Mouse Journey/Mouse_Journey.cpp
+++ CCC/CCC 12/CCC 12 S5 - Mouse Journey/Mouse_Journey.cpp
@@ -4,14 +4,37 @@
 
 using namespace std;
 
-const int Max;
+const int Max = 30;
 
-int N, M;
+int R, C, K, dp[Max];
+bool cats[Max][Max];
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
+    cin >> R >> C;
+    cin >> K;
+    while (K--)
+    {
+        int u, v;
+        cin >> u >> v;
+        cats[u][v] = 1;
+    }
+
+    dp[1] = 1;
+    for (int i = 1; i <= R; ++i)
+    {
+        for (int j = 1; j <= C; ++j)
+        {
+            if (cats[i][j])
+                dp[j] = 0;
+            else
+                dp[j] += dp[j - 1];
+        }
+    }
+
+    cout << dp[C];
     return 0;
 }
