3f18e1ee916f3e57922d92f4d43f4dbdfee4bbd9
WSM-simon <72824231+WSM-simon@users.noreply.github.com>
2022-02-03 17:46:45 -0400

    modified Kaguya Wants to Let Ishigami Pass

--- Mock CCC/Mock CCC 22 2 S1 - Kaguya Wants to Let Ishigami Pass/v1.cpp
+++ Mock CCC/Mock CCC 22 2 S1 - Kaguya Wants to Let Ishigami Pass/v1.cpp
@@ -6,8 +6,10 @@ using namespace std;
 
 const int Max = 1e3 + 3;
 
-int N, M, ans[Max][13], negscore[Max], minmaxnegscore = 1e9 + 3;
+int N, M, ans[Max][13], negscore[Max];
 
+// You actually don't need to change a number to binary, you can just use xor to compare two ints. Such as, one student's answer is 10101(2) = 21
+// the key you generate is 22 = 10110(2), so 21^22 = 00011(2) = 3. For efficency, you can also use __builtin_popcount to count all the 1s in 21^22 quickly.
 vector<int> generate_answer_key(int n)
 {
     vector<int> key;
@@ -28,21 +30,24 @@ int main()
         {
             char c;
             cin >> c;
-            if (c == 'T')
-                ans[i][j] = 1;
+            ans[i][j] = (c == 'T' ? 1 : 0);
         }
 
-    for (int j = 0; j < (1 << M); ++j)
+    int minmaxnegscore = 1e9 + 3;
+    for (int i = 0; i < (1 << M); ++i)
     {
-        int maxnegscore = 0;
         memset(negscore, 0, sizeof(negscore));
-        vector<int> key = generate_answer_key(j);
-        for (int k = 0; k < M; ++k)
+        int maxnegscore = 0;
+        vector<int> key = generate_answer_key(i);
+        for (int r = 0; r < N; ++r)
         {
-            for (int i = 0; i < N; ++i)
+            for (int c = 0; c < M; ++c)
             {
-                if (ans[i][k] != key[k])
-                    negscore[k]++, maxnegscore = max(maxnegscore, negscore[k]);
+                if (ans[r][c] != key[c])
+                {
+                    negscore[r]++;
+                    maxnegscore = max(maxnegscore, negscore[r]);
+                }
             }
         }
         minmaxnegscore = min(minmaxnegscore, maxnegscore);
