91c7710153fcb0e04e812ccd733ba6617384a13f
WSM-simon <72824231+WSM-simon@users.noreply.github.com>
2022-02-15 21:03:46 -0400

    modified model.cpp

--- model.cpp
+++ model.cpp
@@ -1,20 +1,22 @@
 #include <bits/stdc++.h>
 #define ll long long
 #define pii pair<int, int>
+#define pll pair<long long, long long>
+#define f first
+#define s second
+#define ar3 array<int 3>
 
 using namespace std;
 
 const int Max = 1;
 const int inf = 0x3f3f3f3f;
 
-int N;
+int N, M;
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
-    cin >> N;
 
     return 0;
 }
