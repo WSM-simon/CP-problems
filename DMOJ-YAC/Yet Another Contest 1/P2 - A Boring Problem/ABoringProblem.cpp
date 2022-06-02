ba5be561f4d164460a75126864f1ed9cf3f72410
WSM-simon <72824231+WSM-simon@users.noreply.github.com>
2022-06-02 17:14:16 -0300

    okk

--- DMOJ-YAC/Yet Another Contest 1/P2 - A Boring Problem/ABoringProblem.cpp
+++ DMOJ-YAC/Yet Another Contest 1/P2 - A Boring Problem/ABoringProblem.cpp
@@ -1,20 +1,15 @@
 #include <bits/stdc++.h>
 #define ll long long
+#define pll pair<long long, long long>
 
 using namespace std;
 
-const int MxN = 2e5 + 3;
+const int MxN = 2e1 + 3;
 const int MxM = 1e5 + 3;
 
-int N;
-ll badpath = 0;
+ll N, badPath = 0, black[MxN], white[MxN];
+vector<pll> connected;
 bool col[MxN];
-struct node;
-node path[MxN];
-
-struct node {
-    vector<int> white, black;
-};
 
 int main()
 {
@@ -30,20 +25,44 @@ int main()
     for (int i = 1; i <= N - 1; ++i) {
         int t1, t2;
         cin >> t1 >> t2;
+        connected.push_back({t1, t2});
         if (col[t1])
-            path[t2].black.push_back(t1);
+            black[t2]++;
         else
-            path[t2].white.push_back(t1);
+            white[t2]++;
         if (col[t2])
-            path[t1].black.push_back(t2);
+            black[t1]++;
         else
-            path[t1].white.push_back(t2);
-    }
-    for (int i = 1; i <= N; ++i) {
-        if (col[i]) {
-            badpath+=path[i].black.size() * path[i].white.size() + 
-        }
+            white[t1]++;
     }
 
+    // count cases that there are 3 concecutive nodes
+    for (int i = 1; i <= N; ++i) {
+        if (col[i])
+            badPath += white[i] * black[i] + white[i] * (white[i] - 1);
+        else
+            badPath += white[i] * black[i] + black[i] * (black[i] - 1);
+    }
+
+    // count cases that there are 4 concecutive nodes
+    for (auto [n1, n2] : connected) {
+        ll w1 = white[n1], w2 = white[n2];
+        ll b1 = black[n1], b2 = black[n2];
+        if (col[n1])
+            b2--;
+        else
+            w2--;
+
+        if (col[n2])
+            b1--;
+        else
+            w1--;
+
+        if (col[n1] && col[n2]) badPath += w1 * w2;
+        if (! col[n1] && ! col[n2]) badPath += b1 * b2;
+        if ((col[n1] && ! col[n2]) || (! col[n1] && col[n2])) badPath += w1 * b2 + w2 * b1;
+    }
+
+    cout << N * (N - 1) / 2 - (N - 1) - badPath << '\n';
     return 0;
 }
