513c18ea96a685f2eeb23be8fc78a2cb37ffebee
WSM-simon <72824231+WSM-simon@users.noreply.github.com>
2022-06-03 16:50:35 -0300

    #

--- CCC/CCC 19/CCC 19 S3 - Arithmetic Square/ArithmeticSquare.cpp
+++ CCC/CCC 19/CCC 19 S3 - Arithmetic Square/ArithmeticSquare.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 #define ll long long
+#define pll pair<long long, long long>
 
 using namespace std;
 
 const int MxN = 4;
-const int MxM = 1e5 + 3;
 const int inf = 1e18;
 
 ll grid[MxN][MxN], ver[MxN], hor[MxN];
@@ -14,12 +14,19 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
+    fill(begin(grid), end(grid), inf);
     for (int i = 1; i < MxN; ++i) {
         for (int j = 1; j < MxN; ++j) {
-            ll tem;
+            int tem;
             cin >> tem;
-            if (tem ==) ver[]
+            if (cin.fail()) {
+                cin.clear();
+                cin.ignore();
+                continue;
+            }
+            ver[i]++, hor[j], grid[i][j] = tem;
         }
     }
+
     return 0;
 }
---
+++ DMOJ-YAC/Yet Another Contest 1/P3 - Fluke/Fluke.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+const int MxN = 1e5 + 3;
+const int MxM = 1e5 + 3;
+
+int N, T;
+
+void solve()
+{
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N >> T;
+    while (T--) solve();
+    return 0;
+}
