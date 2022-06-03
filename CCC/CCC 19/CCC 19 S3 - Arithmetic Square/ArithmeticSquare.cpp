1a2201a5dc2b90d4a3ef74eb38ec368f3b006eeb
WSM-simon <72824231+WSM-simon@users.noreply.github.com>
2022-06-03 07:23:19 -0300

    added arithemetic Square

---
+++ CCC/CCC 19/CCC 19 S3 - Arithmetic Square/ArithmeticSquare.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+const int MxN = 4;
+const int MxM = 1e5 + 3;
+const int inf = 1e18;
+
+ll grid[MxN][MxN], ver[MxN], hor[MxN];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    for (int i = 1; i < MxN; ++i) {
+        for (int j = 1; j < MxN; ++j) {
+            ll tem;
+            cin >> tem;
+            if (tem ==) ver[]
+        }
+    }
+    return 0;
+}
