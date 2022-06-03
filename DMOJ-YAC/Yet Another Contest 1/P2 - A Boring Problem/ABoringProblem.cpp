dcd850fcdcf7adfdc2f5ea07dd5c2e69ab43b361
WSM-simon <72824231+WSM-simon@users.noreply.github.com>
2022-06-02 21:45:11 -0300

    finished A boring Problem

--- DMOJ-YAC/Yet Another Contest 1/P2 - A Boring Problem/ABoringProblem.cpp
+++ DMOJ-YAC/Yet Another Contest 1/P2 - A Boring Problem/ABoringProblem.cpp
@@ -4,10 +4,10 @@
 
 using namespace std;
 
-const int MxN = 2e1 + 3;
+const int MxN = 2e5 + 3;
 const int MxM = 1e5 + 3;
 
-ll N, badPath = 0, black[MxN], white[MxN];
+ll N, black[MxN], white[MxN];
 vector<pll> connected;
 bool col[MxN];
 
@@ -36,33 +36,29 @@ int main()
             white[t1]++;
     }
 
-    // count cases that there are 3 concecutive nodes
+    // badPath of 2 is equal to the number of connected vertices
+    ll badPath2 = N - 1;
+    // count cases that there are 3 consecutive nodes
+    ll badPath3 = 0;
     for (int i = 1; i <= N; ++i) {
         if (col[i])
-            badPath += white[i] * black[i] + white[i] * (white[i] - 1);
+            badPath3 += white[i] * black[i] + white[i] * (white[i] - 1) / 2;
         else
-            badPath += white[i] * black[i] + black[i] * (black[i] - 1);
+            badPath3 += white[i] * black[i] + black[i] * (black[i] - 1) / 2;
     }
 
-    // count cases that there are 4 concecutive nodes
+    ll badPath4 = 0;
+    // count cases that there are 4 consecutive nodes
     for (auto [n1, n2] : connected) {
         ll w1 = white[n1], w2 = white[n2];
         ll b1 = black[n1], b2 = black[n2];
-        if (col[n1])
-            b2--;
-        else
-            w2--;
 
-        if (col[n2])
-            b1--;
-        else
-            w1--;
-
-        if (col[n1] && col[n2]) badPath += w1 * w2;
-        if (! col[n1] && ! col[n2]) badPath += b1 * b2;
-        if ((col[n1] && ! col[n2]) || (! col[n1] && col[n2])) badPath += w1 * b2 + w2 * b1;
+        if (col[n1] && col[n2]) badPath4 += w1 * w2;
+        if (! col[n1] && ! col[n2]) badPath4 += b1 * b2;
+        if (col[n1] && ! col[n2]) badPath4 += (w1 - 1) * (b2 - 1) + w2 * b1;
+        if (! col[n1] && col[n2]) badPath4 += w1 * b2 + (w2 - 1) * (b1 - 1);
     }
 
-    cout << N * (N - 1) / 2 - (N - 1) - badPath << '\n';
+    cout << N * (N - 1) / 2 - badPath2 - badPath3 - badPath4 << '\n';
     return 0;
 }
