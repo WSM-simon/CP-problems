10aba3e8e6bff220bda45b6a37a6272a03ee36a5
WSM-simon <72824231+WSM-simon@users.noreply.github.com>
2022-12-16 11:38:35 -0400

    added CCC 13

---
+++ CCC/CCC 13/CCC 13 J4 - Time on task/v1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+const int MxN = 1e5 + 3;
+const int MxM = 1e5 + 3;
+
+int N, M;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N;
+    cout << '*' << '\n';
+    N--;
+    for (int i=0;i<N; ++i)
+    {
+        for (int j=0;j<N)
+    }
+    return 0;
+}
---
+++ DMOPC/DMOPC 15 Contest 7/P2 - Not a Wall of Text/v1.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+const int MxN = 1e5 + 3;
+const int MxM = 1e5 + 3;
+
+int N, M;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int cnt = 0;
+    string s;
+    while(cin>>s){
+        cnt++;
+    }
+    cout << cnt << '\n';
+    return 0;
+}
