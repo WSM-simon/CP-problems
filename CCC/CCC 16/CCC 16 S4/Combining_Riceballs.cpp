f5eac0e59eb395de325e4688a8e6b1ec308eea8b
WSM-simon <72824231+WSM-simon@users.noreply.github.com>
2023-02-09 20:29:25 -0400

    ##

--- CCC/CCC 16/CCC 16 S4/Combining_Riceballs.cpp
+++ CCC/CCC 16/CCC 16 S4/Combining_Riceballs.cpp
@@ -8,8 +8,7 @@ const int Max = 403;
 
 int N, dp[Max][Max], ans = 0;
 
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
@@ -18,33 +17,21 @@ int main()
         cin >> dp[i][i], ans = max(ans, dp[i][i]);
 
     for (int i = 0; i < N; ++i)
-        for (int j = i + 1; j < N; ++j)
-            if (dp[i][i] == dp[j][j])
-                dp[i][j] = dp[i][i] + dp[j][j], ans = max(ans, dp[i][j]);
+        if (dp[i][i] == dp[i + 1][i + 1])
+            dp[i][i + 1] = dp[i][i] + dp[i + 1][i + 1], ans = max(ans, dp[i][i + 1]);
 
     for (int i = 0; i < N; ++i)
-        for (int j = j + 2; j < N; ++j)
-            if (dp[i][i] == dp[j][j])
-                dp[i][j] = dp[i][i] + dp[i + 1][i + 1] + dp[j][j], ans = max(ans, dp[i][j]);
+        if (dp[i][i] == dp[i + 2][i + 2])
+            dp[i][i + 2] = dp[i][i] + dp[i + 1][i + 1] + dp[i + 2][i + 2], ans = max(ans, dp[i][i + 2]);
 
     // dp, search from i to i+k, j is another pointer inside [i][i+k]
-    for (int k = 2; k < N; ++k)
-    {
-        for (int i = 0; i + k < N; ++i)
-        {
-            // count for operation 1
-            for (int j = 0; j < k; ++j)
-                if (dp[i][i + j] == dp[i + j + 1][i + k])
-                    dp[i][i + k] = dp[i][i + j] + dp[i + j + 1][i + k], ans = max(ans, dp[i][i + k]);
-
-            // count for operation 2
-            for (int j1 = 0; )
-            {
-                
-            }
+    for (int k = 2; k < N; ++k) {
+        for (int i = 0; i + k < N; ++i) {
+            int p1 = i, p2 = i+k;
+            for (int )
         }
     }
-    
+
     cout << ans;
     return 0;
 }
